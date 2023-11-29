e135fa8bc2fcfe07235eb8a0afe8e01dfbff00b5
soundarraj02 <soundarraj1271@gmail.com>
2023-11-29 18:06:12 +0530

    message

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("soundhar")
+const express = require("express")
+
+const app = express()
+
+app.get('/name', (req,res)=>{
+    console.log("soundhar")
+    res.send({message:"soundhar"})
+})
+
+app.get('/place', (req,res)=>{
+    console.log("soundhar")
+    res.send({message:"salem"})
+})
+
+app.listen(3000)
